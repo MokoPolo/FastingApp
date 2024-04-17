00b6be781bb0bf2156448fab28c2d1ede31aa216
= <=>
2024-04-17 14:51:22 -0400

    Refactor WeatherForecastController and Fasting.API.csproj woohaa

---
+++ Fasting.API/Data/FastingDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Fasting.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fasting.API.Data;
+
+public class FastingDbContext : DbContext
+{
+    public FastingDbContext(DbContextOptions<FastingDbContext> options)
+        : base(options) { }
+
+    public DbSet<Fasting.API.Models.Domain.Fast> Fasts { get; set; }
+
+    public DbSet<Duration> Durations { get; set; }
+}
---
+++ Fasting.API/Models/Domain/Fast.cs
@@ -0,0 +1,11 @@
+﻿namespace Fasting.API.Models.Domain;
+
+public class Fast
+{
+    //public DateTime Start { get; set; }
+
+    //public DateTime End { get; set; }
+
+    //public Duration? Duration { get; set; }
+    public int Id { get; set; }
+}
