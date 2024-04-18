bec1f2808453e4d1b6d16052cac3ee8cf593d66f
= <=>
2024-04-18 15:58:51 -0400

    Add Fast controller StartFastAsync method and DurationDto model

---
+++ Fasting.API/Models/DTO/DurationDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Fasting.API.Models.DTO;
+
+public class DurationDto
+{
+    public int Id { get; set; }
+    public int Length { get; set; }
+
+    public required string Name { get; set; }
+}
