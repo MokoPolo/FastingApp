ba38e000bfe0facd949142de6320db90fb302c24
= <=>
2024-04-22 19:04:22 -0400

    Refactor FastingDbContext to add default duration values and update unit tests

--- Fasting.Api.Test/FastingRepositoryTests.cs
+++ Fasting.Api.Test/FastingRepositoryTests.cs
@@ -1,18 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using Fasting.API;
+﻿using Fasting.API;
 using Fasting.API.Data;
 using Fasting.API.Models.Domain;
 using Microsoft.EntityFrameworkCore;
 using Moq;
 using Xunit;
 
+namespace Fasting.Api.Test;
+
 public class FastingRepositoryTests
 {
     private readonly Mock<FastingDbContext> dbContextMock;
-
     private readonly FastingRepository fastingRepository;
 
     public FastingRepositoryTests()
@@ -22,81 +19,23 @@ public class FastingRepositoryTests
     }
 
     [Fact]
-    public void TestName() { }
-
-    [Fact]
     public async Task CreateAsync_ShouldAddAndSave()
     {
-        var fast = new FastDomain();
+        var fastDomain = new FastDomain();
         var dbSetMock = new Mock<DbSet<FastDomain>>();
 
         dbContextMock.Setup(x => x.Fasts).Returns(dbSetMock.Object);
 
-        var result = await fastingRepository.CreateAsync(fast);
+        var repo = new FastingRepository(dbContextMock.Object);
 
-        dbSetMock.Verify(x => x.AddAsync(fast, It.IsAny<CancellationToken>()), Times.Once);
+        var result = await repo.CreateAsync(fastDomain);
+
+        dbSetMock.Verify(x => x.AddAsync(fastDomain, It.IsAny<CancellationToken>()), Times.Once);
         dbContextMock.Verify(x => x.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
-        Assert.Equal(fast, result);
+
+        Assert.Equal(fastDomain, result);
     }
 
-    // [Fact]
-    // public async Task UpdateAsync_ShouldUpdateAndSave()
-    // {
-    //     var fast = new FastDomain
-    //     {
-    //         Id = 1,
-    //         Start = DateTime.Now,
-    //         End = DateTime.Now.AddHours(1),
-    //         Duration = 1
-    //     };
-    //     var dbSetMock = new Mock<DbSet<FastDomain>>();
-    //     dbSetMock
-    //         .As<IQueryable<FastDomain>>()
-    //         .Setup(m => m.Provider)
-    //         .Returns(
-    //             new List<FastDomain> { fast }
-    //                 .AsQueryable()
-    //                 .Provider
-    //         );
-    //     dbSetMock
-    //         .As<IQueryable<FastDomain>>()
-    //         .Setup(m => m.Expression)
-    //         .Returns(
-    //             new List<FastDomain> { fast }
-    //                 .AsQueryable()
-    //                 .Expression
-    //         );
-    //     dbSetMock
-    //         .As<IQueryable<FastDomain>>()
-    //         .Setup(m => m.ElementType)
-    //         .Returns(
-    //             new List<FastDomain> { fast }
-    //                 .AsQueryable()
-    //                 .ElementType
-    //         );
-    //     dbSetMock
-    //         .As<IQueryable<FastDomain>>()
-    //         .Setup(m => m.GetEnumerator())
-    //         .Returns(
-    //             new List<FastDomain> { fast }
-    //                 .AsQueryable()
-    //                 .GetEnumerator()
-    //         );
-
-    //     dbContextMock.Setup(x => x.Fasts).Returns(dbSetMock.Object);
-
-    //     var updatedFast = new FastDomain
-    //     {
-    //         Id = 1,
-    //         Start = DateTime.Now.AddHours(2),
-    //         End = DateTime.Now.AddHours(3),
-    //         Duration = 1
-    //     };
-    //     var result = await fastingRepository.UpdateAsync(1, updatedFast);
-
-    //     dbContextMock.Verify(x => x.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
-    //     Assert.Equal(updatedFast.Start, result.Start);
-    //     Assert.Equal(updatedFast.End, result.End);
-    //     Assert.Equal(updatedFast.Duration, result.Duration);
-    // }
+    [Fact]
+    public async Task UpdateAsync_ShouldUpdateAndSave() { }
 }
