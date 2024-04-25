3c43ad4877cd7734fe54eb74d8c412703bdefc52
= <=>
2024-04-25 17:15:53 -0400

    Refactor FastingRepository.cs to handle exceptions and log errors

--- Fasting.API/Repositories/FastingRepository.cs
+++ Fasting.API/Repositories/FastingRepository.cs
@@ -16,29 +16,45 @@ public class FastingRepository : IFastingRepository
 
     public async Task<FastDomain> CreateAsync(FastDomain fast)
     {
-        _logger.LogInformation("Creating fast");
-        await _dbContext.Fasts.AddAsync(fast);
-        await _dbContext.SaveChangesAsync();
+        try
+        {
+            _logger.LogInformation("Creating fast");
+            await _dbContext.Fasts.AddAsync(fast);
+            await _dbContext.SaveChangesAsync();
 
-        return fast;
+            return fast;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error creating fast");
+            throw;
+        }
     }
 
     public async Task<FastDomain?> UpdateAsync(int id, FastDomain fast)
     {
-        _logger.LogInformation("Updating fast");
-        var existingFast = _dbContext.Fasts.FirstOrDefault(f => fast.Id == id);
-
-        if (existingFast == null)
+        try
         {
-            return null;
+            _logger.LogInformation("Updating fast");
+            var existingFast = _dbContext.Fasts.FirstOrDefault(f => fast.Id == id);
+
+            if (existingFast == null)
+            {
+                return null;
+            }
+
+            existingFast.Start = fast.Start;
+            existingFast.End = fast.End;
+            existingFast.Duration = fast.Duration;
+
+            await _dbContext.SaveChangesAsync();
+
+            return existingFast;
         }
-
-        existingFast.Start = fast.Start;
-        existingFast.End = fast.End;
-        existingFast.Duration = fast.Duration;
-
-        await _dbContext.SaveChangesAsync();
-
-        return existingFast;
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error updating fast");
+            throw;
+        }
     }
 }
